e658059197fd0d8513c3b64d225e806a6e9fadf6
Kiana Blaine <kiana.blaine1@gmail.com>
2019-02-10 05:28:07 +0000

    Automatically backed up by Learn

---
+++ example.js
@@ -0,0 +1,10 @@
+// declare the variable
+window.word = 'bird'
+ 
+console.log(word) // 'bird'
+ 
+// assign another value to the variable
+word = 'dog'
+ 
+console.log(word) // 'dog'
+//window.word
